2c483fd41517d1e964527cf5a9493922aeb7e4c8
https://github.com/kumargarv05-maker <kumargarv05@gmail.com>
2026-08-10 22:13:03 +0530

    feat: add comment controller and route files for comment management

---
+++ scr/controllers/comment.controller.js
@@ -0,0 +1,11 @@
+import { mongoose } from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import { ApiResponse } from "../utils/ApiResponse";
+import { comment } from "../models/comment.model.js"
+
+
+
+export {
+
+}
