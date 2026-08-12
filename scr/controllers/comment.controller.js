a8627a4946ab164f3a1905a6bb9e54edc14c721d
https://github.com/kumargarv05-maker <kumargarv05@gmail.com>
2026-08-12 22:21:19 +0530

    feat: implement getVideoComment function to retrieve comments for a video

--- scr/controllers/comment.controller.js
+++ scr/controllers/comment.controller.js
@@ -1,10 +1,18 @@
-import { mongoose } from "mongoose";
+import { mongoose, isValidObjectId } from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
 import { comment } from "../models/comment.model.js"
 
 const getVideoComment = asyncHandler( async(req, res) => {
+
+    const { videoId } = req.params
+    const { page = 1, limit = 10 } = req.query
+
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Invalid videoId")
+    }
+
 
 })
 
