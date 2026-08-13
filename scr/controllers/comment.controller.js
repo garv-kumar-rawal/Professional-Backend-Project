d6eeb68a53957f06f17490b7e60f9ad8dbdbeac5
https://github.com/kumargarv05-maker <kumargarv05@gmail.com>
2026-08-13 22:22:48 +0530

    feat: update getVideoComment to retrieve video details and associated comments

--- scr/controllers/comment.controller.js
+++ scr/controllers/comment.controller.js
@@ -2,7 +2,8 @@ import { mongoose, isValidObjectId } from "mongoose";
 import { asyncHandler } from "../utils/asyncHandler";
 import { ApiError } from "../utils/ApiError";
 import { ApiResponse } from "../utils/ApiResponse";
-import { comment } from "../models/comment.model.js"
+import { Comment } from "../models/comment.model.js"
+import { Video } from "../models/comment.model.js"
 
 const getVideoComment = asyncHandler( async(req, res) => {
 
@@ -12,6 +13,20 @@ const getVideoComment = asyncHandler( async(req, res) => {
     if(!isValidObjectId(videoId)){
         throw new ApiError(400, "Invalid videoId")
     }
+
+    const video = await Video.findBy(videoId)
+
+    if(!video){
+        throw new ApiError(404, "Video not Found ")
+    }
+
+    const comment = await Comment.aggregate([
+        {
+            $match: {
+                Video: new mongoose.Types.ObjectId(video)
+            }
+        }
+    ])
 
 
 })
